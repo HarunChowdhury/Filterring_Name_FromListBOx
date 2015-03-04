b87f5593d5015a84198948252cdf9d7fb15c4bb8
harunchowdhury <trishamomo1@gmail.com>
2015-03-04 11:08:04 -0800

    Success

---
+++ StringCheckByCharacter/StringCheckByCharacter/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace StringCheckByCharacter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(129, 28);
+            this.nameTextBox.Multiline = true;
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(132, 27);
+            this.nameTextBox.TabIndex = 0;
+            this.nameTextBox.TextChanged += new System.EventHandler(this.nameTextBox_TextChanged);
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.Location = new System.Drawing.Point(30, 95);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(231, 251);
+            this.nameListBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Name";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 333);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StringCheckByCharacter/StringCheckByCharacter/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringCheckByCharacter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> nameList = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            StreamReader sr = new StreamReader("districts.txt");
+
+            var line = sr.ReadLine();
+
+            while (line != null)
+            {
+                nameList.Add(line);
+
+                line = sr.ReadLine();
+
+            }
+
+            nameListBox.DataSource = nameList;
+
+        }
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            List<string> result = new List<string>();
+
+            foreach (string district in nameList) 
+            {
+                string input = nameTextBox.Text;
+
+                if (district.StartsWith(input))
+                {
+                   
+                    result.Add(district);
+                }
+
+                nameListBox.DataSource = null;
+
+                nameListBox.DataSource = result;
+            }
+
+        }
+
+       
+    }
+}
